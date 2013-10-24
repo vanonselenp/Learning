5ef28ce793f1b72c5304cbe978dbfc8f2f890674
vanonselenp <vanonselen.p@gmail.com>
2013-10-24 20:40:07 +0200

    currently at 444 out of 1659

--- Clojure/cp/README.md
+++ Clojure/cp/README.md
@@ -1,14 +1,4 @@
-# cp
+#Clojure Programming
 
-A Clojure library designed to ... well, that part is up to you.
-
-## Usage
-
-FIXME
-
-## License
-
-Copyright © 2013 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Book from 2012. This is just a place to copy out the code found in the book so
+that I can see it in action and get used to writing Clojure code.
--- Clojure/cp/src/cp/threads.clj
+++ Clojure/cp/src/cp/threads.clj
@@ -1,5 +1,9 @@
 (ns threads.core)
 
+(def d (delay 
+	(println "Running ...")
+	:done))
+
 (defn get-document []
 	{:url "http://www.mozilla.org/about/manifesto.en.html"
 	 :title "The Mozilla Manifesto"
@@ -9,3 +13,83 @@
 (def p (promise))
 
 (deliver p 42)
+
+;futures
+
+(def long-calulation (future (apply + (range 1e9))))
+
+(deref (future (Thread/sleep 5000) :done!)
+	1000
+	:impatient)
+
+(defn get-document2 [id]
+	{:url "http://www.mozilla.org/about/manifesto.en.html"
+	 :title "The Mozilla Manifesto"
+	 :mime "text/html"
+	 :content (future (slurp "http://www.mozilla.org/about/manifesto.en.html"))})
+
+;promises
+
+(def p (promise))
+
+(realized? p)
+
+(deliver p 42)
+
+(def a (promise))
+(def b (promise))
+(def c (promise))
+
+(future
+	(deliver c (+ @a @b))
+	(println "Delivery complete"))
+
+(deliver a 15)
+(deliver b 16)
+
+(defn call-service [arg1 arg2 callback-fn]
+	(future (callback-fn (+ arg2 arg1) (- arg1 arg2))))
+
+(defn sync-fn [async-fn]
+	(fn [& args]
+		(let [result (promise)]
+			(apply async-fn (conj (vec args) #(deliver result %&)))
+			@result)))
+
+((sync-fn call-service) 8 7)
+
+(defn phone-numbers [string]
+	(re-seq #"(\d{3})[\.-]?(\d{3})[\.-]?(\d{4})" string))
+
+(def files (repeat 100
+	(apply str (concat (repeat 1000000 \space) "sunil: 617.555.2937, Betty: 508.555.2218"))))
+
+(time (dorun (map phone-numbers files)))
+
+(time (dorun (pmap phone-numbers files)))
+
+;bad use of pmap
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

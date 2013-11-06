4dd784cc99debf2154f55e84734256f45089df0b
vanonselenp <vanonselen.p@gmail.com>
2013-11-06 23:15:34 +0200

    Removed project euler and put it into its own repo

--- Clojure/webcrawler/src/webcrawler/core.clj
+++ Clojure/webcrawler/src/webcrawler/core.clj
@@ -1,6 +1,9 @@
 (ns webcrawler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(require '[net.cgrand.enlive-html])
+(use '[clojure.string :only (lower-case)])
+(import '(java.net URL MalformedURLException))
+
+(defn- links-from
+	[base-url html]
+	)
--- Project Euler/problem20.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
- 
-(define (factorial x)
-  (if (= x 1) x (* (factorial (- x 1)) x)))
-
-(define (factorial-digit-sum number)
-  (foldl + 0 (map (lambda (x) 
-                    (string->number (list->string (list x)))) 
-                  (string->list (number->string (factorial number))))))

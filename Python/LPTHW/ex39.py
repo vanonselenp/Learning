a037888f965a2add9e96f11f4407390361eccc2b
vanonselenp <vanonselen.p@gmail.com>
2014-02-06 12:30:28 +0200

    39

--- Python/LPTHW/ex39.py
+++ Python/LPTHW/ex39.py
@@ -7,3 +7,25 @@ print person['name']
 del person['city']
 
 print person
+
+states = {
+	'Oregon': 'OR',
+	'Florida': 'FL',
+	'California': 'CA',
+	'New York': 'NY',
+	'Michigan': 'MI',
+}
+
+cities = {
+	'OR':'Portland',
+	'FL':'Jacksonville',
+	'CA':'San Francisco',
+	'NY':'New York City',
+	'MI':'Detroit',
+	'NY':'New Jersey'
+}
+
+print cities['NY']
+print cities
+
+

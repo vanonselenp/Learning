46e930399d492301e6548aa7b2c5b7aa7b5f5bd3
vanonselenp <vanonselen.p@gmail.com>
2015-05-16 00:22:19 +0200

    example exercises of the racket way of doing web development

--- Racket/continue/blog.rkt
+++ Racket/continue/blog.rkt
@@ -1,8 +1,23 @@
 #lang web-server/insta
 
+(struct post (title body))
+
+(define first (post "First post" "this is only a test of the emergency broadcast system"))
+
+(define blog (list first))
+
 (define (start request)
   (response/xexpr 
    `(html 
      (head (title "Blog Example from racket continue"))
-     (body (h1 "under construction")
-           (p "come back later")))))
+     (body ,(render-post first)))))
+
+(define (render-post post)
+   `(div ((class "post"))
+         (h1 ,(post-title post)) 
+         (p ,(post-body post))))
+           
+(define (render-greeting name)
+  (response/xexpr 
+   `(html (head (title "Welcome"))
+          (body (p ,(string-append "hello " name))))))
